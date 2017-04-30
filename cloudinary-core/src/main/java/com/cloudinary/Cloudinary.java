a1ba7ff234497c7b7118ee2344f4e840b6233d45
daniel cohen <daniel@codeinvain.com>
2017-04-30 18:12:34 +0300

    passing cloudinary isntance to search.execute

--- cloudinary-core/src/main/java/com/cloudinary/Cloudinary.java
+++ cloudinary-core/src/main/java/com/cloudinary/Cloudinary.java
@@ -57,7 +57,7 @@ public class Cloudinary {
     }
 
     public Search search() {
-        return new Search();
+        return new Search(this);
     }
 
     public static void registerUploaderStrategy(String className) {
